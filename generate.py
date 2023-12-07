04d309129fb349c55d119fd01deaf632c0101a8f
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-12-07 14:01:41 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,15 +1,14 @@
-## Credits to @blackcatserver github: https://github.com/bcncalling
-
 import os
 import asyncio
 from pyrogram import Client
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
+from hydrogram import Client as HydrogramClient 
 
-API_ID = 24414252 #PUT YOUR OWN API
-API_HASH = "247bc633310a5ce3dc8534c0d496197f" #PUT YOUR API HASH
+API_ID = 24414252  # PUT YOUR OWN API
+API_HASH = "247bc633310a5ce3dc8534c0d496197f"  # PUT YOUR API HASH
 
-async def generate_telethon_session():
+async def telethon_session():
     api_id = API_ID
     api_hash = API_HASH
 
@@ -20,7 +19,7 @@ async def generate_telethon_session():
         print("Your Telethon string session was saved in your saved messages.")
         return session_string
 
-async def generate_pyrogram_session():
+async def pyrogram_session():
     name = "BCN"
     api_id = API_ID
     api_hash = API_HASH
@@ -36,22 +35,38 @@ async def generate_pyrogram_session():
         print("Your Pyrogram string session was saved in your saved messages.")
         return session_string
 
+async def hydrogram_session():
+    api_id = API_ID
+    api_hash = API_HASH
+
+    async with HydrogramClient(api_id, api_hash) as hydrogram:
+        print("Generating Hydrogram String Session...")
+        session_string = await hydrogram.export_session_string()
+        await hydrogram.send_message("me", f"**Your string session:**\n\n`{session_string}`")
+        print("Your Hydrogram string session was saved in your saved messages.")
+        return session_string
+
 async def bcn():
     print("Please Choose an Option:")
     print("1. Generate Telethon Session")
     print("2. Generate Pyrogram V2 Session")
-    choice = input("Enter your choice (1/2): ")
+    print("3. Generate Hydrogram Session") 
+    choice = input("Enter your choice (1/2/3): ")
 
     if choice == "1":
-        telethon_session = await generate_telethon_session()
+        telethon_session = await telethon_session()
         with open("telethon_session.txt", "w") as file:
             file.write(telethon_session)
     elif choice == "2":
-        pyrogram_session = await generate_pyrogram_session()
+        pyrogram_session = await pyrogram_session()
         with open("pyrogram_session.txt", "w") as file:
             file.write(pyrogram_session)
+    elif choice == "3":
+        hydrogram_session = await hydrogram_session()
+        with open("hydrogram_session.txt", "w") as file:
+            file.write(hydrogram_session)
     else:
-        print("🚫 Invalid choice. Please choose 1 or 2.")
+        print("🚫 Invalid choice. Please choose 1, 2, or 3.")
 
 if __name__ == "__main__":
     asyncio.run(bcn())
